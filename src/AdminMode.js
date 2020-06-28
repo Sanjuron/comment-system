af5ea5b428dba6e345c916e7f94bdace10187814
sanjuron <julien_nn@live.fr>
2020-06-28 22:43:17 +0200

    tests delete AdminMode

--- src/AdminMode.js
+++ src/AdminMode.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class AdminMode extends Component {
     state = { 
-        isAdmin: false
+        isAdmin: false,
      }
 
      changeMode = () => { 
@@ -11,6 +11,7 @@ class AdminMode extends Component {
         })
      }
 
+
     render() { 
 
         let button = this.state.isAdmin ? 
@@ -18,12 +19,17 @@ class AdminMode extends Component {
         : <button className="button is-info" onClick={this.changeMode}>Activer le mode d'administration</button>;
 
         let classMessage = this.state.isAdmin ? "message is-danger" : "message is-info";
-       
+
+        let del = this.state.isAdmin ?
+         <button className="delete" onClick={() => {this.props.deleteComment(this.props.state.comment.id)}}>X</button>
+       : null ;
+
         return (         
         <article className={classMessage}>
             <div className="message-body ">
                 {button}
             </div>
+            {del}
         </article>
        );
     }
